4077258c6ac38ce9819e2e8ed15a3a41a93a58cc
mathewaugustine30 <mathewaugustine30@gmail.com>
2021-04-18 23:13:54 +0530

    Initial commit

---
+++ src/test/java/com/mathew/java/springbootcrud/SpringbootCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathew.java.springbootcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
